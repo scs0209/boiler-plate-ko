eedf41613cd939061288d2f27c7010e825342147
Jack <tjdckdtn2463@naver.com>
2022-08-16 17:57:41 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+const config = require('./config/key')
+
+// application/x-www-form-urlencoded를 분석해서 가져오게하는 역할
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json을 분석해서 가져오게하는 역할
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Jack:gk2fnd12@boilerplate.jkymgxq.mongodb.net/?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config. mongoURI)
+  .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~~반갑습니다')
+  res.send('Hello World!~~반가워요')
+})
+
+app.post('/register', (req, res) => {
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
+  // req.body에는 id: hello, password: aaa 의 형태가 있도록 설정해 놓은 것이다.(bodyParser를 이용)
+  const user = new User(req.body)
+// save는 몽고db오는 method
+  user.save((err, userInfo) => {
+    if(err) return res.json({success: false, err})
+    return res.status(200).json({
+      successs: true
+    })
+  })
 })
 
 app.listen(port, () => {
